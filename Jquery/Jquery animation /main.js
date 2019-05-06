afac1508323f29d018f2db652a6af5a33c609d9f
Muhammad Jamal Uddin <jamal30th@gmail.com>
2019-05-06 18:09:21 +0600

    updated js file

--- Jquery/Jquery animation /main.js
+++ Jquery/Jquery animation /main.js
@@ -3,7 +3,23 @@ $('document').ready(function () {
         $('.box').animate({
             left: '-=200px',
         })
-    })  
+
+    }) 
+    $('#right').click(function () {
+        $('.box').animate({
+            right: '-=200px'
+        })
+    })
+    $('#up').click(function () {
+        $('.box').animate({
+            top: '-=200px'
+        })
+    })
+    $('#down').click(function () {
+        $('.box').animate({
+            bottom: '-=200px'
+        })
+    })
 })
 
 
